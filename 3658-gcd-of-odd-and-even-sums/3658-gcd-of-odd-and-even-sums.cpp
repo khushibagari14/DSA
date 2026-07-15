e2bcc8eaf3deb1690d263a1b125385b209e0ee87
khushi bagari <khushibagari3@gmail.com>
2026-07-15 15:06:41 +0530

    Time: 3 ms (16.66%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int oddsum = n*(2*1 +(n-1)*2)/2;
+        int evensum = n*(2*2 +(n-1)*2)/2;
+        return gcd(oddsum , evensum);
         
     }
 };
