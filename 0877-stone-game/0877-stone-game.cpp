a77d8e4c8a905960b7e049283341e061169cdfee
khushi bagari <khushibagari3@gmail.com>
2026-08-02 11:50:09 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-       sort(piles.begin(),piles.end());
-       int alice =0,bob =0;
-       int i =0 ;
-       while(i>0){
-       if(i %2 ==0 ){
-        bob += piles[n-i];
-       }else{
-        alice +=piles[n-i];
-       }
-       }
+    //     int n = piles.size();
+    //    sort(piles.begin(),piles.end());
+    //    int alice =0,bob =0;
+    //    int i =0 ;
+    //    while(i>0){
+    //    if(i %2 ==0 ){
+    //     bob += piles[n-i];
+    //    }else{
+    //     alice +=piles[n-i];
+    //    }
+    //    }
 
-    if(alice < bob) return false;
+    // if(alice < bob) return false;
 
     return true;
         
