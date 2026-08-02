86af10cb5afa4ab094ea408784f1f6e68ce4bbde
khushi bagari <khushibagari3@gmail.com>
2026-08-02 19:35:08 +0530

    Time: 80 ms (52.34%), Space: 110 MB (87.24%) - LeetHub

--- 1686-stone-game-vi/1686-stone-game-vi.cpp
+++ 1686-stone-game-vi/1686-stone-game-vi.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
-       int n = aliceValues.size();
-       vector<pair<int,int>>ans;
-       for(int i =0 ;i<n ;i++){
-            ans.push_back({aliceValues[i] + bobValues[i], i});
-       }
-       sort(ans.rbegin(),ans.rend());
-       int a =0 , b =0;
-       for(int i =0 ;i<n ;i++){
-        int idx = ans[i].second;
-        if(i%2 == 0){
-            a += aliceValues[idx];
-        }else{
-            b += bobValues[idx];
+        int n = aliceValues.size();
+
+        vector<int> idx(n);
+        iota(idx.begin(), idx.end(), 0);
+
+        sort(idx.begin(), idx.end(), [&](int i, int j) {
+            return aliceValues[i] + bobValues[i] >
+                   aliceValues[j] + bobValues[j];
+        });
+
+        int alice = 0, bob = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0)
+                alice += aliceValues[idx[i]];
+            else
+                bob += bobValues[idx[i]];
         }
-       }
-       if(a > b)return 1;
-       if(a <b) return -1;
-       return 0;
-       
+
+        if (alice > bob) return 1;
+        if (alice < bob) return -1;
+        return 0;
     }
 };
